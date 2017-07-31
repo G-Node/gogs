aeeb0a1d9de0c55d0dc39a87e7ff3189e0351398
cgars <christian@stuebeweg50.de>
2017-07-31 14:43:08 +0200

    [GINR] default to gin clone link

--- public/js/gogs.js
+++ public/js/gogs.js
@@ -1387,7 +1387,7 @@ $(document).ready(function () {
                 }
                 break;
             default:
-                $('#repo-clone-https').click();
+                $('#repo-clone-gin').click();
                 break;
         }
     }
