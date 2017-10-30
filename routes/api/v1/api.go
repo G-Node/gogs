6d5581e41fdec50dc8c401d3638f50f1ea91ebbb
cgars <christian@stuebeweg50.de>
2017-10-30 10:13:00 +0100

    [GinS] Gin Search API functionality

--- routes/api/v1/api.go
+++ routes/api/v1/api.go
@@ -21,6 +21,7 @@ import (
 	"github.com/G-Node/gogs/routes/api/v1/org"
 	"github.com/G-Node/gogs/routes/api/v1/repo"
 	"github.com/G-Node/gogs/routes/api/v1/user"
+	"github.com/G-Node/gogs/routes/api/v1/search"
 )
 
 func repoAssignment() macaron.Handler {
@@ -180,6 +181,8 @@ func RegisterRoutes(m *macaron.Macaron) {
 		// Miscellaneous
 		m.Post("/markdown", bind(api.MarkdownOption{}), misc.Markdown)
 		m.Post("/markdown/raw", misc.MarkdownRaw)
+
+		m.Get("/search/:querry", search.Search)
 
 		// Users
 		m.Group("/users", func() {
