aea12c35dfb3c92c350ed10689545daedba81370
cgars <christian@stuebeweg50.de>
2017-11-06 10:50:39 +0100

    [api] re-add file raw download with api

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -316,6 +316,12 @@ var (
 	RunUser      string
 	IsWindows    bool
 	HasRobotsTxt bool
+
+	Search struct {
+		Do        bool
+		IndexUrl  string
+		SearchUrl string
+	}
 )
 
 // DateLang transforms standard language locale name to corresponding value in datetime plugin.
@@ -662,6 +668,8 @@ func NewContext() {
 		log.Fatal(2, "Fail to map API settings: %v", err)
 	} else if err = Cfg.Section("ui").MapTo(&UI); err != nil {
 		log.Fatal(2, "Fail to map UI settings: %v", err)
+	} else if err = Cfg.Section("search").MapTo(&Search); err != nil {
+		log.Fatal(2, "Fail to map Serch settings: %v", err)
 	}
 
 	if Mirror.DefaultInterval <= 0 {
