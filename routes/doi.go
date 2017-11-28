156fe2969ef5f79c948959f86437cd714724b23e
cgars <christian@stuebeweg50.de>
2017-11-28 09:32:13 +0100

    [Doi] Repo admin-ibility needs to be tested

--- routes/doi.go
+++ routes/doi.go
@@ -4,9 +4,15 @@ import (
 	"github.com/G-Node/gogs/pkg/context"
 	"github.com/G-Node/gogs/pkg/setting"
 	"fmt"
+	"net/http"
 )
 
 func RequestDoi(c *context.Context) {
+	if !c.Repo.IsAdmin(){
+		c.Status(http.StatusUnauthorized)
+		return
+	}
+
 	token := c.GetCookie(setting.SessionConfig.CookieName)
 	c.Redirect(fmt.Sprintf("https://doi.gin.g-node.org/?repo=%s&user=%s&token=%s", c.Repo.Repository.FullName(),
 		c.User.Name, token))
