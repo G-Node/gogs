777ae7fe937069adfccc1ba04c37e36d7e9d53fd
cgars <christian@stuebeweg50.de>
2018-01-26 12:07:22 +0100

    [GinR] Fix makefile to work with prefetched gogs

--- docker/build.sh
+++ docker/build.sh
@@ -12,7 +12,6 @@ go get golang.org/x/crypto/bcrypt
 go get github.com/jteeuwen/go-bindata
 go get github.com/G-Node/gin-doi/src
 go get github.com/G-Node/git-module
-
 go get gopkg.in/yaml.v2
 
 cd ${GOPATH}/src/github.com/jteeuwen/go-bindata/go-bindata
@@ -20,6 +19,7 @@ go install
 
 # Build Gogs
 mkdir -p ${GOPATH}/src/github.com/G-Node/
+rm -rf ${GOPATH}/src/github.com/G-Node/gogs
 ln -s /app/gogs/build ${GOPATH}/src/github.com/G-Node/gogs
 cd ${GOPATH}/src/github.com/G-Node/gogs
 # Needed since git 2.9.3 or 2.9.4
@@ -27,7 +27,7 @@ git config --global http.https://gopkg.in.followRedirects true
 make build TAGS="sqlite cert pam"
 
 # Cleanup GOPATH
-rm -r $GOPATH
+#rm -r $GOPATH
 
 
 # Create git user for Gogs
