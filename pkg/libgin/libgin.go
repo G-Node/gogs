37daa90d8bcd8b66bae96ae14dc259074856c699
Achilleas Koutsou <achilleas.k@gmail.com>
2018-11-29 14:36:23 +0100

    Move libgin to external repository

--- pkg/libgin/libgin.go
+++
@@ -1,5 +0,0 @@
-/*
-Package libgin provides functionality for features added for the G-Node GIN service.
-This is a temporary package. Functions and types have been copied from gin-doi and gin-dex services. The plan is for G-Node/GOGS (GIN) to not depend on any code from the added services. Instead, common functionality will be imported from a separate lib package. This subpackage is the first version of that.
-*/
-package libgin
