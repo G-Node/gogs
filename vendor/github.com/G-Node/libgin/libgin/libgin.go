04e7609c1dbfeca00539f7af42c01fdeabbf4dec
Achilleas Koutsou <achilleas.k@gmail.com>
2018-12-06 19:14:23 +0100

    Vendor all dependencies

---
+++ vendor/github.com/G-Node/libgin/libgin/libgin.go
@@ -0,0 +1,8 @@
+/*
+Package libgin provides functionality shared between the G-Node GIN services.
+These include:
+- GIN Web: The G-Node/GOGS fork (https://github.com/G-Node/gogs)
+- GIN DOI: The DOI registration service (https://github.com/G-Node/gin-doi)
+- GIN DEX: The indexing service for GIN (https://github.com/G-Node/gin-dex)
+*/
+package libgin
