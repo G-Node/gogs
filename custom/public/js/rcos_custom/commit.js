4511b2018653ba821605fc06c4f9e0a9ff09ef6d
ivis-kuwata <jumpei.kuwata@ivis.co.jp>
2022-01-17 13:48:31 +0900

    modify: コミットボタン制御を廃止
    参考：https://bloom-pansy-8c3.notion.site/0b1c9062b6004a16ac933dd9854e1b96

--- custom/public/js/rcos_custom/commit.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $("#commit_message").on('input', function (event) {
-        if ($(this).val() === "") {
-            $("#commit").prop('disabled', true)
-        } else {
-            $("#commit").prop('disabled', false)
-        }
-    })
-})
