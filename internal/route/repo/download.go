ae7bbc2bf3823807c4f3a196df9def7c7612bb5b
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-06-07 14:24:58 +0900

    SingleDownloadの修正

--- internal/route/repo/download.go
+++ internal/route/repo/download.go
@@ -15,7 +15,6 @@ import (
 	"github.com/NII-DG/gogs/internal/context"
 	"github.com/NII-DG/gogs/internal/gitutil"
 	"github.com/NII-DG/gogs/internal/tool"
-	logv2 "unknwon.dev/clog/v2"
 )
 
 func serveData(c *context.Context, name string, data []byte) error {
@@ -54,51 +53,9 @@ func ServeBlob(c *context.Context, blob *git.Blob) error {
 }
 
 func SingleDownload(c *context.Context) {
-	logv2.Info("c.Repo.TreePath", c.Repo.TreePath)
-	blob, err := c.Repo.Commit.Blob(c.Repo.TreePath)
-	tree, terr := c.Repo.Commit.TreeEntry(c.Repo.TreePath)
-	if terr != nil {
-		logv2.Error("terr : %v", terr)
-	} else {
-		logv2.Info("tree.IsBlob() : %v", tree.IsBlob())
-		logv2.Info("tree.IsExec() : %v", tree.IsExec())
-		logv2.Info("tree.Mode() : %v", tree.Mode())
-		logv2.Info("tree.Name() : %v", tree.Name())
-		logv2.Info("tree.Type() : %v", tree.Type())
-		logv2.Info("tree.Size() : %v", tree.Size())
-		logv2.Info("tree.Size() : %v", tree.Size())
-		b, e := tree.Blob().Bytes()
-		if e != nil {
-			logv2.Error("tree.Blob().Bytes() : ERR %v", e)
-		}
-		logv2.Info("tree.Blob().Bytes() : %v", string(b))
-	}
-
-	entries, ierr := c.Repo.Commit.Tree.Entries()
-	if ierr != nil {
-		logv2.Error("ierr : %v", ierr)
-	} else {
-		for _, v := range entries {
-			logv2.Info("v : %v", v.Name())
-		}
-	}
-	t, terr := c.Repo.Commit.Subtree("test_data")
-	if ierr != nil {
-		logv2.Error("ierr : %v", ierr)
-	} else {
-
-		entries, ierr = t.Entries()
-		if ierr != nil {
-			logv2.Error("ierr : %v", ierr)
-		} else {
-			for _, v := range entries {
-				logv2.Info("v2 : %v", v.Name())
-			}
-		}
-	}
+	blob, err := getBlobByPath(c.Repo)
 
 	if err != nil {
-		logv2.Error("Repo.Commit.Blob() ERR : %v", err)
 		c.NotFoundOrError(gitutil.NewError(err), "get blob")
 		return
 	}
@@ -107,4 +64,15 @@ func SingleDownload(c *context.Context) {
 		c.Error(err, "serve blob")
 		return
 	}
+}
+
+func getBlobByPath(repo *context.Repository) (*git.Blob, error) {
+	entry, err := repo.Commit.TreeEntry(repo.TreePath)
+	if err != nil {
+		return nil, err
+	}
+	if !entry.IsTree() {
+		return entry.Blob(), nil
+	}
+	return nil, git.ErrNotBlob
 }
